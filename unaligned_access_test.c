f2364f306f3a56227284fbb537acc7d8a10fbf3d
Alexey Novikov <nalexey@amazon.com>
2024-02-11 12:20:45 -0800

    Multythreading

--- unaligned_access_test.c
+++ unaligned_access_test.c
@@ -3,22 +3,24 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/mman.h>
+#include <pthread.h>
 
 #define M (4294967291) //2^32 - 5
 #define A (1588635695)
+#define THREAD_NUM (10)
+#define MAX_THREAD_NAME (10)
 
 #define XSTR(s) STR(s)
 #define STR(s) #s
 #define SHIFT(_x_) ((A*_x_)%M)
-#define RUN_TIMED_LOOP(_seed_, _statement_)\
+#define RUN_TIMED_LOOP(_thread_name_, _seed_, _statement_, _aggregator_)\
   {\
-    printf("'%s'", XSTR(_statement_));\
     uint64_t _tstart_ = clock_gettime_nsec_np(CLOCK_MONOTONIC);\
     for (uint32_t _offset_ = SHIFT(_seed_); _offset_!=_seed_; _offset_ = SHIFT(_offset_)) {\
       _statement_;\
     }\
     uint64_t _tdiff_ = clock_gettime_nsec_np(CLOCK_MONOTONIC) - _tstart_;\
-    printf(" took %llu.%09llu seconds\n", _tdiff_/1000000000, _tdiff_%1000000000);\
+    printf("%s: '%s' %s = %llx, took %llu.%09llu seconds, \n", _thread_name_, XSTR(_statement_), XSTR(_aggregator_), (unsigned long long)_aggregator_, _tdiff_/1000000000, _tdiff_%1000000000);\
   }
 
 void * allocate(size_t size, uint32_t seed) {
@@ -36,50 +38,74 @@ void * allocate(size_t size, uint32_t seed) {
   return p;
 }
 
-void run_test_with_seed(uint32_t seed, const void* chunk4GB) {
-  printf("Running tests with seed %u\n", seed);
-  RUN_TIMED_LOOP(seed, )
+void run_test_with_seed(const char* thread_name, uint32_t seed, const void* chunk4GB) {
+  printf("%s: Running tests with seed %u\n", thread_name, seed);
+  RUN_TIMED_LOOP(thread_name, seed, , 0)
   size_t count = 0;
-  RUN_TIMED_LOOP(seed, count++)
-  printf("COUNT: %zu\n", count);
+  RUN_TIMED_LOOP(thread_name, seed, count++, count)
   char ch = 0;
   char* chunk4GB_bytes = (char*) chunk4GB;
-  RUN_TIMED_LOOP(seed, ch |= chunk4GB_bytes[_offset_])
-  printf("OR: %hhx\n", ch);
+  RUN_TIMED_LOOP(thread_name, seed, ch |= chunk4GB_bytes[_offset_], ch)
   ch = 0;
-  RUN_TIMED_LOOP(seed, ch ^= chunk4GB_bytes[_offset_])
-  printf("XOR: %hhx\n", ch);
+  RUN_TIMED_LOOP(thread_name, seed, ch ^= chunk4GB_bytes[_offset_], ch)
   uint64_t u64 = 0;
   uint64_t* chunk4GB_64bit_words = (uint64_t*) chunk4GB;
-  RUN_TIMED_LOOP(seed, u64 ^= chunk4GB_64bit_words[_offset_/8])
-  printf("XOR: %llx\n", u64);
+  RUN_TIMED_LOOP(thread_name, seed, u64 ^= chunk4GB_64bit_words[_offset_/8], u64)
   u64=0;
   uint64_t* chunk4GB_64bit_words_shift_1byte = (uint64_t*)(chunk4GB+1);
-  RUN_TIMED_LOOP(seed, u64 ^= chunk4GB_64bit_words_shift_1byte[_offset_/8])
-  printf("XOR: %llx\n", u64);
+  RUN_TIMED_LOOP(thread_name, seed, u64 ^= chunk4GB_64bit_words_shift_1byte[_offset_/8], u64)
   u64=0;
   uint64_t* chunk4GB_64bit_words_shift_2bytes = (uint64_t*)(chunk4GB+1);
-  RUN_TIMED_LOOP(seed, u64 ^= chunk4GB_64bit_words_shift_2bytes[_offset_/8])
-  printf("XOR: %llx\n", u64);
+  RUN_TIMED_LOOP(thread_name, seed, u64 ^= chunk4GB_64bit_words_shift_2bytes[_offset_/8], u64)
   u64=0;
   uint64_t* chunk4GB_64bit_words_shift_4bytes = (uint64_t*)(chunk4GB+1);
-  RUN_TIMED_LOOP(seed, u64 ^= chunk4GB_64bit_words_shift_4bytes[_offset_/8])
-  printf("XOR: %llx\n", u64);
+  RUN_TIMED_LOOP(thread_name, seed, u64 ^= chunk4GB_64bit_words_shift_4bytes[_offset_/8], u64)
   ch=0;
-  RUN_TIMED_LOOP(seed, ch ^= __atomic_load_n(chunk4GB_bytes + _offset_, __ATOMIC_RELAXED))
-  printf("XOR: %hhx\n", ch);
+  RUN_TIMED_LOOP(thread_name, seed, ch ^= __atomic_load_n(chunk4GB_bytes + _offset_, __ATOMIC_RELAXED), ch)
+}
+
+struct thread_info {
+  pthread_t thread_id;
+  char thread_name[MAX_THREAD_NAME];
+  uint32_t seed;
+  const void* chunk;
+};
+
+static void* run_test_with_seed_thrd_wrapper(void* tinfo) {
+  struct thread_info* args = (struct thread_info*)tinfo;
+  run_test_with_seed(args->thread_name, args->seed, args->chunk);
+  return NULL;
+}
+
+void run_tests_with_seed(uint32_t seed, const void* chunk) {
+  run_test_with_seed("main", seed, chunk);
+
+  pthread_attr_t tattr;
+  assert(pthread_attr_init(&tattr) == 0);
+  struct thread_info tinfo[THREAD_NUM];
+  for (size_t tnum = 0; tnum < THREAD_NUM; tnum++) {
+    assert(snprintf(tinfo[tnum].thread_name, MAX_THREAD_NAME, "Thread %zu", tnum) > 0);
+    tinfo[tnum].seed = seed;
+    tinfo[tnum].chunk = chunk;
+    assert(pthread_create(&tinfo[tnum].thread_id, &tattr, &run_test_with_seed_thrd_wrapper, &tinfo[tnum]) == 0);
+  }
+  assert(pthread_attr_destroy(&tattr) == 0);
+  for (size_t tnum = 0; tnum < THREAD_NUM; tnum++) {
+    assert(pthread_join(tinfo[tnum].thread_id, NULL) == 0);
+  }
 }
 
 int main(int argc, char* argv[]) {
+  setlinebuf(stdout);
   printf("Allocating and filling 4 GB chunk on heap...\n");
   void* chunk4GB = allocate((size_t)(UINT32_MAX)+4, 42);
   if (argc == 1) {
-    run_test_with_seed(42, chunk4GB);
+    run_tests_with_seed(42, chunk4GB);
     return 0;
   }
 
   for (int i = 1; i < argc; i++) {
-    run_test_with_seed(((uintptr_t)argv[i]), chunk4GB);
+    run_tests_with_seed(((uintptr_t)argv[i]), chunk4GB);
   }
 
   return 0;
